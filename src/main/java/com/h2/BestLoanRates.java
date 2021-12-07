3c39f543d526b7651f020b51f659edb1ad855780
stabakov <seid.tab@gmail.com>
2021-12-07 19:07:11 +0100

    Module 1 completed

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -10,7 +10,7 @@ public class BestLoanRates {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name;
-        System.out.println("Enter your name:");
+        System.out.println("Enter your name");
         name = scanner.nextLine();
         System.out.println("Hello " + name);
         System.out.println("Enter the loan term (in years)");
